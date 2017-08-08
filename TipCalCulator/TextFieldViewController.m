ee1dd7b0a43d41fcd2bd896d395ae39f3ade1839
harandian <hharandian@gmail.com>
2017-08-08 01:03:10 -0700

    Adding bill splitting option

--- TipCalCulator/TextFieldViewController.m
+++ TipCalCulator/TextFieldViewController.m
@@ -18,6 +18,9 @@
 @property (nonatomic) float tipPercent;
 @property (weak, nonatomic) IBOutlet UIButton *setTipAt15Perc;
 @property (weak, nonatomic) IBOutlet UIButton *setTipAt17Perc;
+@property (weak, nonatomic) IBOutlet UIButton *setTopTo20Perc;
+@property (weak, nonatomic) IBOutlet UITextField *enterTipAmount;
+@property (nonatomic) BOOL displayCustomAmount;
 
 @end
 
@@ -28,16 +31,13 @@
     // Do any additional setup after loading the view.
     
     
-    
-    
-    
-    
     self.enterBillAmount.delegate = self;
+    self.enterTipAmount.delegate = self;
     
     self.enterBillAmount.backgroundColor = [UIColor grayColor];
     
     [self.enterBillAmount becomeFirstResponder];
-
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -47,35 +47,50 @@
 
 
 
-- (BOOL)textFieldShouldReturn:(UITextField *)enterBillAmount {
 
-    [self.enterBillAmount resignFirstResponder];
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
     
-//    
-//    self.billAmount = [self.enterBillAmount.text floatValue] * self.tipPercent;
-//    
-//    self.TipAmount.text = [NSString stringWithFormat:@"%.2f",self.billAmount];
-//    
-//    self.billAmount = ([self.enterBillAmount.text floatValue] * self.tipPercent) + [self.enterBillAmount.text integerValue];
-//    ;
-//    
-//    NSLog(@"%.2f",self.billAmount);
-//    
-//    self.calculatedFullAmount.text = [NSString stringWithFormat:@"%.2f",self.billAmount];//self.billAmount;
-//    
+    
+    if (textField.tag == 1) {
+        
+        [self.enterBillAmount resignFirstResponder];
+        
+    } else {
+                
+        [self.enterTipAmount resignFirstResponder];
+        
+        self.displayCustomAmount = YES;
+        
+        self.tipPercent = [self.enterTipAmount.text floatValue]/100;
+        
+        self.billAmount = [self.enterBillAmount.text floatValue] * self.tipPercent;
+        
+        self.TipAmount.text = [NSString stringWithFormat:@"%.2f",self.billAmount];
+        
+        self.billAmount = ([self.enterBillAmount.text floatValue] * self.tipPercent) + [self.enterBillAmount.text integerValue];
+        
+        
+        NSLog(@"%.2f",self.billAmount);
+        
+        self.calculatedFullAmount.text = [NSString stringWithFormat:@"%.2f",self.billAmount];//self.billAmount;
+        
+    }
     return YES;
+    
+    
+    
+    
 }
-
 
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 - (IBAction)setTo15Perc:(id)sender {
     
@@ -93,8 +108,8 @@
     
     self.calculatedFullAmount.text = [NSString stringWithFormat:@"%.2f",self.billAmount];//self.billAmount;
     
-
-
+    
+    
 }
 
 - (IBAction)setTo17Perc:(id)sender {
@@ -113,14 +128,14 @@
     
     self.calculatedFullAmount.text = [NSString stringWithFormat:@"%.2f",self.billAmount];//self.billAmount;
     
-
-
+    
+    
 }
 
 - (IBAction)setTo25perc:(id)sender {
     
     self.tipPercent = 0.20;
-
+    
     
     self.billAmount = [self.enterBillAmount.text floatValue] * self.tipPercent;
     
